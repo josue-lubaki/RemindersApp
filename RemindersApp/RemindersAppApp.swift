d50a5f96f80a9a7ee9195160869752a3aea9a12a
Josue Lubaki <josuelubaki@gmail.com>
2024-03-17 13:40:24 -0400

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Josue Lubaki on 2024-03-17.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
