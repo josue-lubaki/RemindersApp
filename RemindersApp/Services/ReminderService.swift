88620380bbee0d7947b124ebff3b52f81470f3bc
Josue Lubaki <josuelubaki@gmail.com>
2024-03-19 15:24:18 -0400

    fix displaying reminder of today

--- RemindersApp/Services/ReminderService.swift
+++ RemindersApp/Services/ReminderService.swift
@@ -71,10 +71,10 @@ class ReminderService {
         switch statType {
             case .all :
                 request.predicate = NSPredicate(format: "isCompleted = false")
-            case .today :
+            case .today:
                 let today = Date()
-                let tomorrow = Calendar.current.date(byAdding: .day, value : 1, to: today)
-                request.predicate = NSPredicate(format: "(reminderDate >= %@) AND (reminderDate < %@)", today as NSDate, tomorrow! as NSDate)
+                let tomorrow = Calendar.current.date(byAdding: .day, value: 1, to: today)
+                request.predicate = NSPredicate(format: "(reminderDate BETWEEN {%@, %@}) OR (reminderTime BETWEEN {%@, %@})", today as NSDate, tomorrow! as NSDate, today as NSDate, tomorrow! as NSDate)
             case .scheduled :
                 request.predicate = NSPredicate(format: "(reminderDate != nil OR reminderTime != nil) AND isCompleted = false")
             case .completed :
